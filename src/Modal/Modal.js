09734bffd37f88ee74a6b166849a8a7b8f77f3d3
Nini24 <43237508+nini24@users.noreply.github.com>
2022-05-18 10:23:11 +0100

    styling modal component

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -6,15 +6,18 @@ const Modal = ({isShowing,hide}) => isShowing ? ReactDOM.createPortal(
     <React.Fragment>
          <div className="modal-overlay"/>
     <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="modal">
+      <div className="modal font-['Gilroy'] ">
         <div className="modal-header">
           <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <p>
-          Hello, I'm a modal.
-        </p>
+        <p className="font-medium text-center text-lg mt-2">Hi there, please fill in these details to book a spot</p>
+
+      <form className="mt-8 text-center mb-10 ">
+            <input type='text' placeholder='Enter your email address' required className="px-8 py-3 border border-slate-700 placeholder-slate-500 text-sm font-medium" />
+            <button className="text-sm px-2 py-3 bg-black text-white border border-black">Book</button>
+      </form>
       </div>
     </div>
     </React.Fragment>,document.body

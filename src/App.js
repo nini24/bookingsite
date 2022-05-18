9993fcb0325a5ace912454d36a410f567a2aa333
Nini24 <43237508+nini24@users.noreply.github.com>
2022-05-18 09:32:11 +0100

    added modal component and styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import logo from './logoweb.svg'
 import menu from './menu.svg'
 import imageone from './imageone.png'
 import imagetwo from './imagetwo.png'
+import imagethre from './imagethre.png'
+import Modal from './Modal/Modal'
+import useModal from './Modal/useModal'
+
 
 import './App.css';
 
 function App() {
+  const {isShowing,toggle} = useModal()
   return (
     <div className="App font-['Gilroy']">
       <header className='mt-10'>
@@ -21,12 +26,15 @@ function App() {
       <section>
         <p className='font-bold lg:text-6xl w-6/12 mx-auto mt-16'>Book a crafts session in two minutes.</p>
         <p className='font-medium text-lg mt-6'>Learn to make beautiful artworks with <span className='font-bold'>Seyi</span></p>
-      <button className='font-bold px-8 py-4 bg-black text-white mt-16 hover:outline-none hover:bg-slate-900'>Book a session</button>
+      <button onClick={toggle} className='font-bold px-8 py-4 bg-black text-white mt-16 hover:outline-none hover:bg-slate-900'>Book a session</button>
+      <Modal isShowing={isShowing} hide={toggle} />
       </section>
       <section className='bg-black mt-20'>
       <div className='pt-4 grid grid-cols-3'>
         <div className=''><img className='mt-10 lg:w-40' src={imageone} /></div>
         <div className=''><img className='mt-10 lg:w-60' src={imagetwo} /></div>
+        <div className=''><img className='mt-10 lg:w-40' src={imagethre} /></div>
+
 
       </div>
       </section>

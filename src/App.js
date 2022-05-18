b98a435f22b756138cd77ad697a194f096b351bf
Nini24 <43237508+nini24@users.noreply.github.com>
2022-05-18 12:36:27 +0100

    styling modal component

--- src/App.js
+++ src/App.js
@@ -16,24 +16,27 @@ function App() {
       <header className='mt-10'>
         <div className='flex items-center justify-between'>
           <div>
-          <img src={logo} alt='logo' className='ml-12' />
+          <img src={logo} alt='logo' className='ml-6 lg:ml-12' />
           </div>
-          <div className='mr-12'>
+          <div className=' mr-6 lg:mr-12'>
           <button><img src={menu} /></button>
           </div>
         </div>
       </header>
       <section>
-        <p className='font-bold lg:text-6xl w-6/12 mx-auto mt-16'>Book a crafts session in two minutes.</p>
-        <p className='font-medium text-lg mt-6'>Learn to make beautiful artworks with <span className='font-bold'>Seyi</span></p>
-      <button onClick={toggle} className='font-bold px-8 py-4 bg-black text-white mt-16 hover:outline-none hover:bg-slate-900'>Book a session</button>
+        
+        <p className='font-bold text-3xl lg:text-6xl w-10/12 lg:w-6/12 mx-auto mt-20 lg:mt-16'>Book a crafts session in two minutes.</p>
+        <div className='slide-left'>
+        <p className='font-medium lg:text-lg mt-3 lg:mt-6'>Learn to make beautiful artworks with <span className='font-bold'>Seyi</span></p>
+        </div>
+      <button onClick={toggle} className='font-bold px-8 py-4 bg-black text-white mt-11 lg:mt-16 hover:outline-none hover:bg-slate-900'>Book a session</button>
       <Modal isShowing={isShowing} hide={toggle} />
       </section>
       <section className='bg-black mt-20'>
-      <div className='pt-4 grid grid-cols-3'>
-        <div className=''><img className='mt-10 lg:w-40' src={imageone} /></div>
-        <div className=''><img className='mt-10 lg:w-60' src={imagetwo} /></div>
-        <div className=''><img className='mt-10 lg:w-40' src={imagethre} /></div>
+      <div className='pt-4 grid grid-cols-3 h-96 gap-9 lg:ml-32'>
+        <div className='w-60 lg:w-80'><img className='w-24 mt-20 lg:mt-10 lg:w-40' src={imageone} /></div>
+        <div className='w-60 lg:w-96'><img className='mt-20 lg:mt-10 lg:w-60' src={imagetwo} /></div>
+        <div className='w-60 '><img className=' mt-20 lg:mt-10 lg:w-40' src={imagethre} /></div>
 
 
       </div>
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import  ReactDOM  from "react-dom";
+import { useForm } from "react-hook-form";
 
 
-const Modal = ({isShowing,hide}) => isShowing ? ReactDOM.createPortal(
+
+
+
+const Modal = ({isShowing,hide}) => {
+
+    const [email,setEmail] = useState('')
+   const handleChange = e => console.log(e.target.value)
+    const onSubmit = data =>console.log(data)
+return (
+isShowing ? ReactDOM.createPortal(
     <React.Fragment>
          <div className="modal-overlay"/>
     <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
@@ -14,14 +24,14 @@ const Modal = ({isShowing,hide}) => isShowing ? ReactDOM.createPortal(
         </div>
         <p className="font-medium text-center text-lg mt-2">Hi there, please fill in these details to book a spot</p>
 
-      <form className="mt-8 text-center mb-10 ">
-            <input type='text' placeholder='Enter your email address' required className="px-8 py-3 border border-slate-700 placeholder-slate-500 text-sm font-medium" />
+      <form className="mt-8 text-center mb-10" onSubmit={onSubmit}>
+            <input type='email' name="email" onChange={handleChange}   placeholder='Enter your email address' required className="px-8 py-3 border border-slate-700 placeholder-slate-500 text-sm font-medium" />
             <button className="text-sm px-2 py-3 bg-black text-white border border-black">Book</button>
       </form>
       </div>
     </div>
     </React.Fragment>,document.body
-):null
-
+):null)
+}
 
 export default Modal
